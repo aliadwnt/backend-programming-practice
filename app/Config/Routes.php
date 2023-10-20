c32deda0a96f8ba7580bfcbe998fe47400032760
aliadwnt <dewantoalia@gmail.com>
2023-10-20 10:57:23 +0700

    add API

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,7 @@ $routes->get('edit-product/(:any)', 'ProductController::getProduct/$1');
 $routes->get('edit-product/(:any)', 'ProductController::updateProduct/$1');
 $routes->post('product-update/(:any)', 'ProductController::updateProduct/$1');
 $routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
+$routes->group('api', function($routes){
+    $routes->get('products','ProductController::readProductApi');
+    $routes->get('product/(:any)','ProductController::getProductApi/$1');
+});
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
+use CodeIgniter\API\ResponseTrait;
 
 class ProductController extends BaseController{
+use ResponseTrait;
 
     private $product;
 
@@ -21,6 +23,39 @@ class ProductController extends BaseController{
 
         return view('product',  $data);
     }
+
+    public function readProductsApi(){
+        $products = $this->product->findAll();
+
+        return $this->respond(
+            [
+                'code' => 200,
+                'status' => "OK",
+                'data' => $products
+            ]
+        );
+    }
+    
+    public function getProductApi($id){
+        $product = $this->product->where('id', $id)->first();
+    if (!$product) {
+        $this->response->setStatusCode(404);
+        return $this->response->setJSON(
+            [
+                'code' => 200,
+                'status' => "NOT FOUND",
+                'data' => 'product not found'
+            ]
+            );
+    }
+
+    return $this->respond([
+        'code' => 200,
+        'status' => "OK",
+        'data' => $product
+    ]);
+}
+
     public function insertProductORM()
     {
         $data = [
